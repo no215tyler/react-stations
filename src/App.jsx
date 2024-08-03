d658c91cd3f45dc9dca68c214bcbada1614b3060
no215tyler <no.215.tyler@gmail.com>
2024-08-03 22:47:07 +0900

    station3: サイトの説明と犬の画像を表示しよう

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ export const App = () => {
   return (
     <header>
       <h2>Sample App</h2>
+      <p>犬の画像を表示するサイトです。</p>
+      <img src="https://images.dog.ceo/breeds/rottweiler/n02106550_3528.jpg" />
     </header>
   )
 }
