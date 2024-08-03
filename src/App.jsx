35f87717d187eee5d106c2577624399355fe6511
no215tyler <no.215.tyler@gmail.com>
2024-08-03 22:55:15 +0900

    station4: useStateを使ってみよう

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 // DO NOT DELETE
 
+import { useState } from 'react'
 import './App.css'
 
 /**
  * @type {() => JSX.Element}
  */
 export const App = () => {
+  const [dogUrl, setDogUrl] = useState(
+    'https://images.dog.ceo/breeds/rottweiler/n02106550_3528.jpg',
+  )
   return (
     <header>
       <h2>Sample App</h2>
       <p>犬の画像を表示するサイトです。</p>
-      <img src="https://images.dog.ceo/breeds/rottweiler/n02106550_3528.jpg" />
+      <img src={dogUrl} />
     </header>
   )
 }
