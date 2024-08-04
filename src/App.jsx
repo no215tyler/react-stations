14a8e561334a8bcb679966735fa0bae1a4c25a14
no215tyler <no.215.tyler@gmail.com>
2024-08-04 11:58:36 +0900

    station8: コンポーネント分割とProps

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,17 @@
 // DO NOT DELETE
 
-import { useCallback, useState } from 'react'
 import './App.css'
+import Header from './Header'
+import Description from './Description'
 
 /**
  * @type {() => JSX.Element}
  */
 export const App = () => {
-  const [dogUrl, setDogUrl] = useState(
-    'https://images.dog.ceo/breeds/rottweiler/n02106550_3528.jpg',
-  )
-  const handleUpdateUrl = useCallback(async () => {
-    const res = await fetch('https://dog.ceo/api/breeds/image/random')
-    const url = await res.json()
-    setDogUrl(url.message)
-  }, [])
   return (
     <div className="main_wrapper">
-      <header className="header">
-        <h1>Dog Photo App</h1>
-      </header>
-      <p className="description">犬の画像を表示するサイトです。</p>
-      <div className="image">
-        <img src={dogUrl} />
-      </div>
-      <br />
-      <button onClick={handleUpdateUrl} className="button">
-        更新
-      </button>
+      <Header />
+      <Description />
     </div>
   )
 }
--- src/Description.jsx
+++ src/Description.jsx
@@ -1,7 +1,26 @@
 // @ts-check
+import { useCallback, useState } from 'react'
+import DogImage from './DogImage'
 
 export const Description = () => {
-  return <></>
+  const [dogUrl, setDogUrl] = useState(
+    'https://images.dog.ceo/breeds/rottweiler/n02106550_3528.jpg',
+  )
+  const handleUpdateUrl = useCallback(async () => {
+    const res = await fetch('https://dog.ceo/api/breeds/image/random')
+    const url = await res.json()
+    setDogUrl(url.message)
+  }, [])
+  return (
+    <>
+      <p className="description">犬の画像を表示するサイトです。</p>
+      <DogImage imageUrl={dogUrl} />
+      <br />
+      <button onClick={handleUpdateUrl} className="button">
+        更新
+      </button>
+    </>
+  )
 }
 
 export default Description
