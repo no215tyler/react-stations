e4ad40e5ea0630ea2b3e47f0ba4338f59a85165d
no215tyler <no.215.tyler@gmail.com>
2024-08-04 14:03:40 +0900

    station9: useEffectを使ってみよう

--- src/DogListContainer.jsx
+++ src/DogListContainer.jsx
@@ -1,6 +1,29 @@
 // @ts-check
 
+import { useEffect, useState } from 'react'
+
 export const DogListContainer = () => {
+  const fetchDogTypes = async () => {
+    const res = await fetch('https://dog.ceo/api/breeds/list/all')
+    const dogType = await res.json()
+    const dogTypesArray = Object.keys(dogType.message)
+    return dogTypesArray
+  }
+
+  const [breeds, setBreeds] = useState([])
+
+  useEffect(() => {
+    console.log('マウント')
+    const getDogTypes = async () => {
+      const dogTypesArray = await fetchDogTypes()
+      setBreeds(dogTypesArray)
+    }
+    getDogTypes()
+
+    return () => {
+      console.log('アンマウント')
+    }
+  }, [])
   return <></>
 }
 
